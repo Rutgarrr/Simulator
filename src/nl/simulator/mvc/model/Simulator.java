916dff660546c73d12d133b3649eec38840d164b
Rutger <rutgerschroder94@gmail.com>
2016-04-12 14:00:25 +0200

    Refactored tick

--- src/nl/simulator/mvc/model/Simulator.java
+++ src/nl/simulator/mvc/model/Simulator.java
@@ -21,6 +21,11 @@ public class Simulator {
 
     private int tickPause = 100;
 
+    Random random;
+
+    // Here gets the average of cars per hour stored in
+    int averageNumberOfCarsPerHour;
+
     private int weekDayArrivals = 50; // average number of arriving cars per hour
     private int weekendArrivals = 90; // average number of arriving cars per hour
 
@@ -28,13 +33,14 @@ public class Simulator {
     private int paymentSpeed = 10; // number of cars that can pay per minute
     private int exitSpeed = 9; // number of cars that can leave per minute
 
-    private Time time;
+    private Time time; // Variable to store the time in
 
     public Simulator() {
         entranceCarQueue = new CarQueue();
         paymentCarQueue = new CarQueue();
         exitCarQueue = new CarQueue();
         time = new Time();
+        random = new Random();
         simulatorView = new SimulatorView(3, 6, 30, this); //TODO Remove!
     }
 
@@ -44,30 +50,29 @@ public class Simulator {
         }
     }
 
-    public void setMaxPassHolder(int maxPassHolder)
-    {
-        this.maxPassHolder = maxPassHolder;
-    }
-
-    // TODO Refactor tick()
-    private void tick() {
-        //update time(duh)
-        int[] fullTime = time.updateTime();
-
-        Random random = new Random();
-
-        // Get the average number of cars that arrive per hour.
-        int averageNumberOfCarsPerHour = fullTime[2] < 5
+    // Calculates average number of cars per hour and stores it in the averageNumberOfCars variable
+    // From tick
+    private void setAvgNumberOfCarsPerHour(int day){
+        this.averageNumberOfCarsPerHour = day < 5
                 ? weekDayArrivals
                 : weekendArrivals;
+    }
 
+    // Calculates cars per minute (a little random) and returns this number
+    // From tick
+    private int carsPerMinute(){
         // Calculate the number of cars that arrive this minute.
         double standardDeviation = averageNumberOfCarsPerHour * 0.1;
         double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
         int numberOfCarsPerMinute = (int)Math.round(numberOfCarsPerHour / 60);
+        return numberOfCarsPerMinute;
+    }
 
-        // Add the cars to the back of the queue.
-        for (int i = 0; i < numberOfCarsPerMinute; i++) {
+    // Add the cars to the back of the queue.
+    // From tick
+    private void addCarsToQueue(){
+
+        for (int i = 0; i < carsPerMinute(); i++) {
             if(random.nextInt(100) < 5 && passCarAmount < maxPassHolder){
                 Car car = new PassHolderCar();
                 entranceCarQueue.addCar(car);
@@ -77,8 +82,11 @@ public class Simulator {
                 entranceCarQueue.addCar(car);
             }
         }
+    }
 
-        // Remove car from the front of the queue and assign to a parking space.
+    // Remove car from the front of the queue and assign to a parking space.
+    private void assignToParkingSpot(){
+
         for (int i = 0; i < enterSpeed; i++) {
             Car car = entranceCarQueue.removeCar();
             if (car == null) {
@@ -92,11 +100,12 @@ public class Simulator {
                 car.setMinutesLeft(stayMinutes);
             }
         }
+    }
 
-        // Perform car park tick.
-        simulatorView.tick();
+    // Add leaving cars to the payment queue.
+    // But if the car is an instance of passholder then add this car to the exit queue
+    private void addCarToExitQueue(){
 
-        // Add leaving cars to the exit queue.
         while (true) {
             Car car = simulatorView.getFirstLeavingCar();
             if (car == null) {
@@ -111,8 +120,11 @@ public class Simulator {
                 paymentCarQueue.addCar(car);
             }
         }
+    }
 
-        // Let cars pay.
+    // Let cars pay.
+    private void letCarsPay(){
+
         for (int i = 0; i < paymentSpeed; i++) {
             Car car = paymentCarQueue.removeCar();
             if (car == null) {
@@ -122,8 +134,11 @@ public class Simulator {
             simulatorView.removeCarAt(car.getLocation());
             exitCarQueue.addCar(car);
         }
+    }
 
-        // Let cars leave.
+    // Let cars leave.
+    private void letCarsLeave(){
+
         for (int i = 0; i < exitSpeed; i++) {
             Car car = exitCarQueue.removeCar();
             if (car == null) {
@@ -135,15 +150,43 @@ public class Simulator {
             }
             // Bye!
         }
+    }
 
-        // Update the car park view.
-        simulatorView.updateView();
-
+    // Lets the program pause for a time that is stored in the variable tickPause in miliseconds
+    private void pause(){
         // Pause.
         try {
             Thread.sleep(tickPause);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    // set the maximum amount of passholders
+    public void setMaxPassHolder(int maxPassHolder)
+    {
+        this.maxPassHolder = maxPassHolder;
+    }
+
+    // TODO Refactor tick()
+    private void tick() {
+        //update time(duh)
+        int[] fullTime = time.updateTime();
+        setAvgNumberOfCarsPerHour(fullTime[2]);
+        addCarsToQueue();
+        assignToParkingSpot();
+
+
+        // Perform car park tick.
+        simulatorView.tick();
+
+        addCarToExitQueue();
+        letCarsPay();
+        letCarsLeave();
+
+        // Update the car park view.
+        simulatorView.updateView();
+
+        pause();
     }
 }
