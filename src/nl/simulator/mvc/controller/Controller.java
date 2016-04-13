b23a0232b74c8b2c211849e5c6488218f74c0910
Bugstorm <Bugstorm@users.noreply.github.com>
2016-04-13 23:51:03 +0200

    Added start/stop (with thread)

--- src/nl/simulator/mvc/controller/Controller.java
+++ src/nl/simulator/mvc/controller/Controller.java
@@ -12,6 +12,8 @@ public class Controller extends AbstractController implements ActionListener{
     private Simulator simulator;
     private JButton step1;
     private JButton step100;
+    private JButton start;
+    private JButton stop;
 
     public Controller(Simulator simulator) {
         super(simulator);
@@ -19,11 +21,17 @@ public class Controller extends AbstractController implements ActionListener{
 
         step1 = new JButton("1 step");
         step100 = new JButton("100 steps");
+        start = new JButton("Start");
+        stop = new JButton("Stop");
         step1.addActionListener(this);
         step100.addActionListener(this);
+        start.addActionListener(this);
+        stop.addActionListener(this);
 
         this.add(step1);
         this.add(step100);
+        this.add(start);
+        this.add(stop);
 
         setVisible(true);
     }
@@ -34,6 +42,10 @@ public class Controller extends AbstractController implements ActionListener{
             simulator.run(1);
         } else if(e.getSource() == step100) {
             simulator.run(100);
+        } else if(e.getSource() == start) {
+            simulator.start();
+        } else if(e.getSource() == stop) {
+            simulator.stop();
         }
     }
 }
